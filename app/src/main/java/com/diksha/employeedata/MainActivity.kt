3758d34a2c9aa7ed1c543c682eb65dae5dcbcabd
dkashyapiiii <dikshakashyap679@gmail.com>
2021-07-10 15:13:23 +0530

    fourth

--- app/src/main/java/com/diksha/employeedata/MainActivity.kt
+++ app/src/main/java/com/diksha/employeedata/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.diksha.employeedata
 
 import android.Manifest
-import android.content.ActivityNotFoundException
-import android.content.Intent
+import android.content.*
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.net.ConnectivityManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.provider.OpenableColumns
 import android.util.Log
+import android.webkit.MimeTypeMap
 import android.widget.Button
 import android.widget.Toast
 import androidx.annotation.RequiresApi
@@ -21,6 +26,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.diksha.employeedata.Adapter.EmployeeAdapter
+import com.diksha.employeedata.Dao.EmployeeDao
+import com.diksha.employeedata.Database.EmployeeDatabase
 import com.diksha.employeedata.Modal.Employee
 import com.diksha.employeedata.ModelClass.EmployeeModel
 import com.diksha.employeedata.ModelClass.Maindata
@@ -33,7 +40,6 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.io.*
 import java.util.*
-import java.util.stream.Stream
 
 
 class MainActivity : AppCompatActivity() {
@@ -45,28 +51,41 @@ class MainActivity : AppCompatActivity() {
     private var actorAdapter: EmployeeAdapter? = null
     private var mSwipeRefreshLayout: SwipeRefreshLayout? = null
     var jsonb: Button? = null
+    var permission: Boolean? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        initilization()
+        recyclerView = findViewById(R.id.recycler)
+        jsonb = findViewById(R.id.jsonfile)
+        mSwipeRefreshLayout = findViewById(R.id.pullToRefresh)
+        actorRespository = EmployeeRespository(application)
+        actorList = ArrayList()
+        actorViewModal = ViewModelProvider(this).get(ActorViewModal::class.java)
 
         checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE)
+
         if (isNetworkConnected) {
             networkRequest()
         } else {
             Toast.makeText(this, "Please check your connection", Toast.LENGTH_SHORT).show()
         }
 
-        jsonb!!.setOnClickListener { showFileChooser() }
+        jsonb!!.setOnClickListener {   if(permission==false)
+            checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE)
+        else
+            showFileChooser()
+        }
+
 
         mSwipeRefreshLayout!!.setOnRefreshListener {
             val handler = Handler()
             handler.postDelayed({
                 if (mSwipeRefreshLayout!!.isRefreshing) {
                     if (isNetworkConnected) {
-                        responseRequest()
+                       // responseRequest()
+                        //addadptor()
+                            networkRequest()
                         mSwipeRefreshLayout!!.isRefreshing = true
                     } else {
                         Toast.makeText(
@@ -84,7 +103,11 @@ class MainActivity : AppCompatActivity() {
             actorAdapter!!.getAllMyModels(actorList)
             Log.d("main", "onChanged: $actorList")
         })
+
+        addadptor()
+
     }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -94,9 +117,11 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == STORAGE_PERMISSION_CODE) {
             if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                permission=true
                 Toast.makeText(this@MainActivity, "Storage Permission Granted", Toast.LENGTH_SHORT)
                     .show()
             } else {
+                permission=false
                 Toast.makeText(this@MainActivity, "Storage Permission Denied", Toast.LENGTH_SHORT)
                     .show()
             }
@@ -115,55 +140,24 @@ class MainActivity : AppCompatActivity() {
                 storagePermissionCode
             )
         } else {
+
 //            Toast.makeText(this@MainActivity, "Permission already granted", Toast.LENGTH_SHORT)
 //                .show()
         }
     }
 
-    private fun initilization() {
-        recyclerView = findViewById(R.id.recycler)
-        jsonb = findViewById(R.id.jsonfile)
-        mSwipeRefreshLayout = findViewById(R.id.pullToRefresh)
+    private fun addadptor() {
+
         var layoutManager = LinearLayoutManager(this@MainActivity)
         recyclerView!!.layoutManager = layoutManager
         recyclerView!!.setHasFixedSize(true)
-        actorRespository = EmployeeRespository(application)
         actorList = ArrayList()
         actorAdapter = EmployeeAdapter(this, actorList as ArrayList<Employee>)
-        actorViewModal = ViewModelProvider(this).get(ActorViewModal::class.java)
+        actorAdapter!!.notifyDataSetChanged()
+     //   actorViewModal = ViewModelProvider(this).get(ActorViewModal::class.java)
     }
 
 
-    private fun responseRequest() {
-        val retrofit = Retrofit()
-        val call = retrofit.api.allActors
-        if (call != null) {
-            call.enqueue(object : Callback<EmployeeModel?> {
-                override fun onResponse(
-                    call: Call<EmployeeModel?>,
-                    response: Response<EmployeeModel?>
-                ) {
-                    if (response.isSuccessful) {
-                        mSwipeRefreshLayout!!.isRefreshing = false
-                        actorAdapter = EmployeeAdapter(this@MainActivity, savetoparentmodel(response.body()!!.banner1!!))
-                        recyclerView!!.adapter = actorAdapter
-                        actorAdapter!!.notifyDataSetChanged()
-                        mSwipeRefreshLayout!!.isRefreshing = false
-                        actorRespository!!.insert(response.body()!!.banner1?.let {
-                            savetoparentmodel(
-                                it
-                            )
-                        })
-                        Log.d("main", "onResponse: " + response.body())
-                    }
-                }
-
-                override fun onFailure(call: Call<EmployeeModel?>, t: Throwable) {
-                    Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
-                }
-            })
-        }
-    }
 
     private fun networkRequest() {
         val retrofit = Retrofit()
@@ -177,11 +171,20 @@ class MainActivity : AppCompatActivity() {
                     if (response.isSuccessful) {
                         mSwipeRefreshLayout!!.isRefreshing = false
 
+
                         actorRespository!!.insert(response.body()!!.banner1?.let {
                             savetoparentmodel(
                                 it
                             )
                         })
+                        actorViewModal!!.allActor.observe(this@MainActivity, { actorList ->
+                            recyclerView!!.adapter = actorAdapter
+                            actorAdapter!!.getAllMyModels(actorList)
+                            Log.d("main", "onChanged: $actorList")
+                        })
+
+                        addadptor()
+
                         Log.d("main", "onResponse: " + response.body())
                     }
                 }
@@ -209,12 +212,23 @@ class MainActivity : AppCompatActivity() {
                 employeeModeldata[i].jobholder?.role,
                 employeeModeldata[i].education?.degree,
                 employeeModeldata[i].education?.institution
+//                        employeeModeldata[0].firstname,
+//                employeeModeldata[0].lastname,
+//                employeeModeldata[0].age,
+//                employeeModeldata[0].gender,
+//                employeeModeldata[0].picture,
+//                employeeModeldata[0].jobholder?.exp,
+//                employeeModeldata[0].jobholder?.organization,
+//                employeeModeldata[0].jobholder?.role,
+//                employeeModeldata[0].education?.degree,
+//                employeeModeldata[0].education?.institution
             )
             data.add(ld)
         }
         Log.d("data", data.toString())
         return data
     }
+
 
     private val isNetworkConnected: Boolean
         private get() {
@@ -239,25 +253,46 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         when (requestCode) {
             FILE_SELECT_CODE -> if (resultCode == RESULT_OK && data!!.data != null) {
                 try {
+                    var name :String ? =""
                     val uri = data.data
                     Log.d("data", uri.toString());
+                    data.data?.let { returnUri ->
+                        contentResolver.query(returnUri, null, null, null, null)
+                    }?.use { cursor ->
 
-                    Parsetest(readTextFromUri(uri!!));
-                    Toast.makeText(this, uri.toString(), Toast.LENGTH_SHORT).show()
+                        val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                        cursor.moveToFirst()
+                        name=cursor.getString(nameIndex)
+                    }
+                    if(name!!.endsWith(".json")){
+
+                        Parsetest(readTextFromUri(uri!!))
+                        Toast.makeText(this,  "Data Populated Successfully!!", Toast.LENGTH_LONG).show()
+
+                    }else{
+
+                        Toast.makeText(this,  "Unsupported file format. Please select a .json file", Toast.LENGTH_LONG).show()
+                    }
 
                 } catch (e: FileNotFoundException) {
                     Log.d("error", e.message.toString());
                     Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                }catch (e :IOException){
+                    Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+
                 }
             }
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
+
+
 
     @Throws(IOException::class)
     private fun readTextFromUri(uri: Uri): String? {
